6cdb979e3eab042f3fa010525fcd7ca2aaa6a054
raman tehlan <ramantehlan@gmail.com>
2018-08-03 17:29:06 +0530

    minor changes

---
+++ intro.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# This is the introduction to first shell script
+
+# Echo is just to print somethin on the screen
+echo "Hello World!" # this is a inline comment
+
+echo "To make a script eXecutable"
+echo "chmod +x intro.sh"
+ 
